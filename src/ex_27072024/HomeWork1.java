b19a2a0934413dc953aa88ca9f5e4910a4ac9deb
Akash Dhembare <akash.dhembare777@gmail.com>
2024-08-01 14:51:50 +0530

    Renamed

---
+++ src/ex_27072024/HomeWork1.java
@@ -0,0 +1,24 @@
+package ex_27072024;
+
+import java.util.Arrays;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+
+        //How to Find the Second Highest Salary in the Array?
+        int[] salaries = {7, 10, 4, 13, 25, 18, 9, 8, 17};
+
+        int max=salaries[0];
+
+        for (int i = 0; i < salaries.length; i++) {
+            if(salaries[i]>max){
+                max=salaries[i];
+            }
+        }
+        System.out.println("Maximum salary is -> "+max);
+        Arrays.sort(salaries);
+        int second_highest_salary=salaries[salaries.length-2];
+        System.out.println("Second Highest Salary is -> "+ second_highest_salary);
+
+    }
+}
---
+++ src/ex_27072024/HomeWork3.java
@@ -0,0 +1,19 @@
+package ex_27072024;
+
+import java.util.Scanner;
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the height of pyramid");
+        int height=sc.nextInt();
+
+        for (int i = height; i >0 ; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}

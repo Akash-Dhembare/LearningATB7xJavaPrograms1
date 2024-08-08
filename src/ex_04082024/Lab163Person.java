359d4438654e1c8ea24eb73c6877d5fd0960dc02
Akash Dhembare <akash.dhembare777@gmail.com>
2024-08-08 09:39:59 +0530

    Renamed

---
+++ src/ex_04082024/Lab163Person.java
@@ -0,0 +1,15 @@
+package ex_04082024;
+
+public class Lab163Person {
+    String name;
+
+    //Special method
+
+    Lab163Person(){
+        System.out.println("Hi, I will be called, Object is created!");
+    }
+
+    void talk(){
+        System.out.println("I am talking");
+    }
+}
---
+++ src/ex_04082024/Lab164Car.java
@@ -0,0 +1,14 @@
+package ex_04082024;
+
+public class Lab164Car {
+    Lab164Car(){
+        System.out.println("Starting");
+        //What is the purpose of this?
+        //Here I can write anything code which I want
+        //to execute before starting
+
+        System.out.println("Reading Data from Mysql");
+        System.out.println("Reading Data from csv");
+        System.out.println("Reading Data from eXCEL");
+    }
+}

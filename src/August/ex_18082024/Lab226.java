727984a1c740d638d6fe1516c4b454b236203db7
Akash Dhembare <akash.dhembare777@gmail.com>
2024-08-20 14:15:53 +0530

    Renamed

---
+++ src/August/ex_18082024/Lab226.java
@@ -0,0 +1,18 @@
+package August.ex_18082024;
+
+public class Lab226 extends Object{
+    public static void main(String[] args) {
+        String name="Aramod"; //pramod
+        String name2="Pramod"; //pramod
+        if(name.equalsIgnoreCase(name2)){
+            System.out.println("Equal");
+        }
+        else {
+            System.out.println("Not Equal");
+        }
+        // final -> constant
+        // finally -> block of code with the try and catch
+        // finalize -> Object class function - threading use case
+
+    }
+}

d0575b3ff72f2f4bedd95efd7fc2369fe31b9d1c
Akash Dhembare <akash.dhembare777@gmail.com>
2024-08-21 14:42:33 +0530

    Renamed

---
+++ src/August/ex_18082024/CollectionFramework/Lab233.java
@@ -0,0 +1,16 @@
+package August.ex_18082024.CollectionFramework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab233 {
+    public static void main(String[] args) {
+        List<Integer> marks=new ArrayList();
+        marks.add(100);
+        System.out.println(marks);
+        //marks.add("Pramod");
+        //marks.add(true);
+        //Collection - Interface
+        //Collections - Class
+    }
+}
